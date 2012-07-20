3deab000ef006127cb24a79bccddf98392080717
Fred Bricon <fbricon@gmail.com>
2012-07-21 00:13:02 +0200

    Format groovy script
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- org.eclipse.m2e.wtp.site/src/main/assembly/fixupdate.groovy
+++ org.eclipse.m2e.wtp.site/src/main/assembly/fixupdate.groovy
@@ -1,29 +1,32 @@
 def basedir = project.basedir.canonicalPath 
-
-def ant = new AntBuilder();   // create an antbuilder
 def contentJar = basedir  + "/target/site/content.jar"
 def contentDir = basedir  + "/target/content.jar/"
-println 'unzipping content.jar'
+
+println 'Unzipping content.jar'
+
+def ant = new AntBuilder();   // create an antbuilder
 ant.unzip(  src: contentJar, dest:contentDir,  overwrite:"true")
-println ' Doing some black magic'
+
+println 'Modify content.xml to make m2e-wtp updatable from m2eclipse-wtp'
 
 File contentXml =  new File(contentDir, "content.xml")
 def root = new XmlParser().parseText(contentXml.text)
-
 def feature = root.units.unit.find{ it.@id=="org.eclipse.m2e.wtp.feature.feature.group"}
 def version = feature.@version 
-println "found " + feature.@id + '-' + version 
+println "Found " + feature.@id + '-' + version 
 
 def matchDefinition = "providedCapabilities.exists(pc | pc.namespace == 'org.eclipse.equinox.p2.iu' && (pc.name == 'org.maven.ide.wtp.feature.feature.group' || pc.name == 'org.eclipse.m2e.wtp.feature.feature.group' && pc.version < '${version}'))"
-println matchDefinition 
+println "Adding match attribute :" + matchDefinition 
 
 feature.update.@match = matchDefinition
 feature.update[0].attributes().remove('id')
 feature.update[0].attributes().remove('range')
 
+println 'Overwriting content.xml'
+
 def writer = new StringWriter()
 new XmlNodePrinter(new PrintWriter(writer)).print(root)
 contentXml.write(writer.toString())
 
-println 'zipping customized content.jar'
+println 'Zipping back customized content.jar'
 ant.zip(  destFile: contentJar, baseDir:contentDir)
