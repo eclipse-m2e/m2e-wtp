34d93201f0e3062daaf2820076779508818514cd
Fred Bricon <fbricon@gmail.com>
2012-09-07 15:57:17 +0200

    385746 : fix license formatting issue
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- org.eclipse.m2e.wtp.site/src/main/assembly/fixupdate.groovy
+++ org.eclipse.m2e.wtp.site/src/main/assembly/fixupdate.groovy
@@ -10,23 +10,22 @@ ant.unzip(  src: contentJar, dest:contentDir,  overwrite:"true")
 println 'Modify content.xml to make m2e-wtp updatable from m2eclipse-wtp'
 
 File contentXml =  new File(contentDir, "content.xml")
+
+//Identify feature Version
 def root = new XmlParser().parseText(contentXml.text)
 def feature = root.units.unit.find{ it.@id=="org.eclipse.m2e.wtp.feature.feature.group"}
 def version = feature.@version 
 println "Found " + feature.@id + '-' + version 
 
-def matchDefinition = "providedCapabilities.exists(pc | pc.namespace == 'org.eclipse.equinox.p2.iu' && (pc.name == 'org.maven.ide.wtp.feature.feature.group' || pc.name == 'org.eclipse.m2e.wtp.feature.feature.group' && pc.version < '${version}'))"
-println "Adding match attribute :" + matchDefinition 
 
-feature.update.@match = matchDefinition
-feature.update[0].attributes().remove('id')
-feature.update[0].attributes().remove('range')
+//Replace update match definition
+String originalStatement = "<update id='org.eclipse.m2e.wtp.feature.feature.group' range='[0.0.0,$version)' severity='0'/>"
+matchDefinition = "providedCapabilities.exists(pc | pc.namespace == \"org.eclipse.equinox.p2.iu\" &amp;&amp; (pc.name == \"org.maven.ide.eclipse.wtp.feature.feature.group\" || pc.name == \"org.eclipse.m2e.wtp.feature.feature.group\" &amp;&amp; pc.version &lt; \"${version}\"))"
+String newStatement = "<update match='$matchDefinition' severity='0'/>"
+String newContentText = contentXml.text.replace(originalStatement, newStatement)
 
 println 'Overwriting content.xml'
-
-def writer = new StringWriter()
-new XmlNodePrinter(new PrintWriter(writer)).print(root)
-contentXml.write(writer.toString())
+contentXml.text = newContentText
 
 println 'Zipping back customized content.jar'
 ant.zip(  destFile: contentJar, baseDir:contentDir)
