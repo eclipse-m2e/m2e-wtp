870b4f09817b24780e1fbab8d6468f54d62ab8da
Fred Bricon <fbricon@gmail.com>
2013-05-07 14:23:08 +0200

    407398 : Add support for JCA 1.7
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- org.eclipse.m2e.wtp/src/org/eclipse/m2e/wtp/RarPluginConfiguration.java
+++ org.eclipse.m2e.wtp/src/org/eclipse/m2e/wtp/RarPluginConfiguration.java
@@ -23,7 +23,12 @@ import org.eclipse.jst.j2ee.project.facet.IJ2EEFacetConstants;
 import org.eclipse.jst.jee.util.internal.JavaEEQuickPeek;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
 import org.eclipse.m2e.wtp.internal.Messages;
+import org.eclipse.osgi.util.NLS;
+import org.eclipse.wst.common.project.facet.core.IFacetedProject;
 import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
+import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * maven-rar-plugin configuration model.
@@ -38,11 +43,16 @@ import org.eclipse.wst.common.project.facet.core.IProjectFacetVersion;
  */
 public class RarPluginConfiguration {
 
+  private static final Logger LOG = LoggerFactory.getLogger(RarPluginConfiguration.class) ;
+
   private static final String RAR_DEFAULT_CONTENT_DIR = "src/main/rar";  //$NON-NLS-1$
 
   private static final String RA_XML = "META-INF/ra.xml"; //$NON-NLS-1$
 
   private static final int JCA_1_6_ID = 16;//Exists in WTP >= 3.2 only
+
+  private static final int JCA_1_7_ID = 17;//Exists in WTP >= 3.5 only
+
 
   final Plugin plugin;
   
@@ -124,6 +134,9 @@ public class RarPluginConfiguration {
               case JCA_1_6_ID:
                 //Don't create a static 1.6 facet version, it'd blow up WTP < 3.2
                 return IJ2EEFacetConstants.JCA_FACET.getVersion("1.6");//only exists in WTP version >= 3.2 //$NON-NLS-1$
+              case JCA_1_7_ID:
+                //Don't create a static 1.7 facet version, it'd blow up WTP < 3.5
+                return IJ2EEFacetConstants.JCA_FACET.getVersion("1.7");//only exists in WTP version >= 3.5 //$NON-NLS-1$
             }
           } finally {
             is.close();
@@ -135,6 +148,17 @@ public class RarPluginConfiguration {
         }
       }
 
+      IProject project = rarFacade.getProject();
+      //If no rar.xml found, don't change existing facet version
+      try {
+          IFacetedProject fProject = ProjectFacetsManager.create(project);
+          if (fProject != null && fProject.hasProjectFacet(IJ2EEFacetConstants.JCA_FACET)) {
+            return fProject.getProjectFacetVersion(IJ2EEFacetConstants.JCA_FACET);
+          }
+        } catch (Exception e) {
+          LOG.warn(NLS.bind(Messages.Error_Reading_Project_Facet, project.getName()), e); 
+        }      
+      
       //If no ra.xml found and the project depends and WTP >= 3.2, then set connector facet to 1.6
       //TODO see if other conditions might apply to differentiate JCA 1.6 from 1.5
       return IJ2EEFacetConstants.JCA_FACET.getVersion("1.6"); //$NON-NLS-1$
